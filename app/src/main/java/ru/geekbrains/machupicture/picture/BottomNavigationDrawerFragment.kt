2ff31e526500e63b1764fd7e0bf7668498bb8b97
EugeneKur <ew.kurilov@gmail.com>
2022-03-10 09:51:41 +0300

    homework7

--- app/src/main/java/ru/geekbrains/machupicture/picture/BottomNavigationDrawerFragment.kt
+++ app/src/main/java/ru/geekbrains/machupicture/picture/BottomNavigationDrawerFragment.kt
@@ -1,5 +1,6 @@
 package ru.geekbrains.machupicture.picture
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -8,6 +9,7 @@ import android.widget.Toast
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import kotlinx.android.synthetic.main.bottom_navigation_layout.*
 import ru.geekbrains.machupicture.R
+import ru.geekbrains.machupicture.recycler.RecyclerActivity
 
 class BottomNavigationDrawerFragment : BottomSheetDialogFragment() {
 
@@ -24,8 +26,16 @@ class BottomNavigationDrawerFragment : BottomSheetDialogFragment() {
 
         navigation_view.setNavigationItemSelectedListener { menuItem ->
             when (menuItem.itemId) {
-                R.id.navigation_one -> Toast.makeText(context, "1", Toast.LENGTH_SHORT).show()
+                R.id.navigation_one -> Toast.makeText(context, "Recycler", Toast.LENGTH_SHORT).show()
                 R.id.navigation_two -> Toast.makeText(context, "2", Toast.LENGTH_SHORT).show()
+                R.id.navigation_three -> activity?.let {
+                    startActivity(
+                        Intent(
+                            it,
+                            RecyclerActivity::class.java
+                        )
+                    )
+                }
             }
             true
         }
